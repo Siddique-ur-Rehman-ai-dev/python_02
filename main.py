e9874033b18ff5fc1e15dce1ed0630336b0ab7cd
Siddique ur rehman <16360@cityuniversity.edu.pk>
2026-06-20 22:54:56 +0500

    Changes are made in branch

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ def main():
     print("Hello from python-02!")
     print('Its practice for Git and Github Actions! ')
     print('SOme new features were added')
+    print('These chnages are made in Adding_comments1 branch')
 
 
 if __name__ == "__main__":
